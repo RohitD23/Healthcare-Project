36c37a00756120715337ed469b004abac05b4956
detonator22 <ashutoshinde23@gmail.com>
2022-09-17 14:58:52 +0530

    Added doctors list

---
+++ src/Doctors.js
@@ -0,0 +1,30 @@
+export const Doctors = [
+    {
+        'name' : 'Dr. Shruti Ganesh Despande',
+        'education' : 'MBBS, MS - General Surgery',
+        'experience' : 20,
+        'rating': 5,
+        'location' : 'Sasun Hospital, Pune',
+        'fees' : 500,
+        'availability' : 'Today'
+    },
+    {
+        'name' : 'Dr. Kavita Akelesh Yadav',
+        'education' : 'MBBS, Neurology',
+        'experience' : 12,
+        'rating': 5,
+        'location' : 'Nobel Hospital, Mumbai',
+        'fees' : 1250,
+        'availability' : 'Today'
+    },
+    {
+        'name' : 'Dr. Emily Panders',
+        'education' : 'MS - Dentist',
+        'experience' : 24,
+        'rating': 5,
+        'location' : 'Healthcare Clinic, Mumbai',
+        'fees' : 800,
+        'availability' : 'Tuesday'
+    }
+    
+]

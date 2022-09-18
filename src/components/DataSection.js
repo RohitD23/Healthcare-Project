58ec74036cd1fc50f1d3b6087d7838c57b598bf0
RohitD23 <rohitdesai2304@gmail.com>
2022-09-18 19:22:58 +0530

    Added data section

--- src/components/DataSection.js
+++ src/components/DataSection.js
@@ -2,7 +2,37 @@ import React from "react";
 import styled from "styled-components";
 
 export default function DataSection() {
-  return <Heading>Pristyn Care in Numbers</Heading>;
+  return (
+    <>
+      <Heading>Pristyn Care in Numbers</Heading>
+      <Data>
+        <DataContent>
+          <DataNumber>200K+</DataNumber>
+          <DataInfo>Happy Patients</DataInfo>
+        </DataContent>
+        <DataContent>
+          <DataNumber>150+</DataNumber>
+          <DataInfo>Clinics</DataInfo>
+        </DataContent>
+        <DataContent>
+          <DataNumber>45+</DataNumber>
+          <DataInfo>Cities</DataInfo>
+        </DataContent>
+        <DataContent>
+          <DataNumber>60K+</DataNumber>
+          <DataInfo>Surgeries</DataInfo>
+        </DataContent>
+        <DataContent>
+          <DataNumber>400+</DataNumber>
+          <DataInfo>Doctors</DataInfo>
+        </DataContent>
+        <DataContent>
+          <DataNumber>800+</DataNumber>
+          <DataInfo>Hospitals</DataInfo>
+        </DataContent>
+      </Data>
+    </>
+  );
 }
 
 const Heading = styled.div`
@@ -10,7 +40,36 @@ const Heading = styled.div`
   font-weight: 700;
   opacity: 1;
   text-align: center;
-  padding-top: 8rem;
-  margin-bottom: 8rem;
+  padding-top: 4rem;
+  margin-bottom: 5rem;
   color: #505257;
+`;
+
+const Data = styled.div`
+  display: grid;
+  grid-template-columns: 20rem 20rem 20rem;
+  grid-template-rows: 3.5rem 3.5rem 3.5rem;
+  gap: 10rem 10rem;
+  justify-content: center;
+`;
+
+const DataContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  line-height: 3.5rem;
+`;
+
+const DataNumber = styled.div`
+  color: #505257;
+  font-size: 4.8rem;
+  font-weight: 800;
+  text-align: center;
+`;
+
+const DataInfo = styled.div`
+  color: #505257;
+  font-size: 2rem;
+  font-weight: 400;
+  text-align: center;
 `;
